eedd85a514d4a85be785846dfd690cd1787e4990
daheller <daheller@daheller-HP>
2017-02-14 09:39:02 +0100

    Änderung daheller
    Test

--- src/de/dpma/pumaz/bvs/MainApp.java
+++ src/de/dpma/pumaz/bvs/MainApp.java
@@ -1,7 +1,14 @@
 package de.dpma.pumaz.bvs;
 
-import javafx.event.ActionEvent;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MainApp {
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
