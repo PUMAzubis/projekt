1c20d13e0f69b1874b60bccd255aa20a4598725a
KeBöhmer <KeBhmer@192.168.111.105>
2017-02-14 16:39:29 +0100

    Merge Fehler bei mir ausgebessert
    Kenneth

--- src/de/dpma/pumaz/bvs/MainApp.java
+++ src/de/dpma/pumaz/bvs/MainApp.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
+import de.dpma.pumaz.bvs.dao.BuchDAO;
 import de.dpma.pumaz.bvs.dao.DatabaseConnection;
+import de.dpma.pumaz.bvs.model.Buch;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,73 +15,69 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-	
+
 	private Stage primaryStage;
-	
+
 	private BorderPane rootLayout;
-	
+
 	Logger log = Logger.getLogger(MainApp.class.getName());
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception, ClassNotFoundException, SQLException {
-		
+
 		this.primaryStage = primaryStage;
 		initRootLayout();
 		showLogin();
 	}
-	
+
 	private void showLogin() {
-		
+
 		try {
-			
+
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/Login.fxml"));
-			
+
 			AnchorPane login;
 			login = (AnchorPane) loader.load();
 			rootLayout.setCenter(login);
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	private void initRootLayout() {
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-			
+
 			rootLayout = (BorderPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
+		launch(args);
+
 		DatabaseConnection dbcon = null;
 		try {
 			dbcon = new DatabaseConnection();
-			
-		}
-		catch (ClassNotFoundException | SQLException e) {
+			BuchDAO buchDao = new BuchDAO(dbcon.getConnection());
+			buchDao.insertBuch(new Buch("Tolles Buch", "Toller Autor", 2016, "0000000000", 1));
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				dbcon.closeConnection();
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		launch(args);
-		
 	}
 }
