336189163591fbc6388dd89f8a7daa17f6f4a531
daheller <daheller@daheller-HP>
2017-02-14 09:49:32 +0100

    daheller
    Test

---
+++ src/de/dpma/pumaz/bvs/CryApp.java
@@ -0,0 +1,17 @@
+package de.dpma.pumaz.bvs;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CryApp extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
