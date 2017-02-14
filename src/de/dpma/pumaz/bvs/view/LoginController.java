f847083fd12a40cb0507b2971f2d5b701978380d
KeBöhmer <KeBhmer@KennethBoehmer>
2017-02-14 10:14:20 +0100

    LoginController Beenden eingefügt
    Kenneth

--- src/de/dpma/pumaz/bvs/view/LoginController.java
+++ src/de/dpma/pumaz/bvs/view/LoginController.java
@@ -1,6 +1,9 @@
 package de.dpma.pumaz.bvs.view;
 
+import java.sql.SQLException;
 import java.util.logging.Logger;
+
+import de.dpma.pumaz.bvs.dao.DatabaseConnection;
 
 public class LoginController {
 	
@@ -8,6 +11,14 @@ public class LoginController {
 	
 	public void handleExit() {
 		
+		DatabaseConnection data = null;
+		try {
+			data.closeConnection();
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.exit(0);
 	}
 	
