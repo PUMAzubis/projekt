2541ac9a2b95a0b581742a200013fcd8e1dc3955
FlSeidl <FlSeidl@192.168.111.144>
2017-02-14 11:09:30 +0100

    Database Verbindung richtig eingestellt
    Flo

--- src/de/dpma/pumaz/bvs/MainApp.java
+++ src/de/dpma/pumaz/bvs/MainApp.java
@@ -1,22 +1,35 @@
 package de.dpma.pumaz.bvs;
 
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
+import de.dpma.pumaz.bvs.dao.DatabaseConnection;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-	
+
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	Logger log = Logger.getLogger(MainApp.class.getName());
-	
-	public static void main(String[] args) {
-		
-		System.out.println();
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		DatabaseConnection dbcon = null;
+		try {
+			dbcon = new DatabaseConnection();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dbcon.closeConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/de/dpma/pumaz/bvs/dao/DatabaseConnection.java
+++ src/de/dpma/pumaz/bvs/dao/DatabaseConnection.java
@@ -6,26 +6,27 @@ import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class DatabaseConnection {
-	
+
 	public Connection con = null;
-	
+
 	Logger log = Logger.getLogger(DatabaseConnection.class.getName());
-	
+
 	public DatabaseConnection() throws ClassNotFoundException, SQLException {
-		log.info("Die verbindung zur Datenbank wird gestartet");
+		log.info("Die Verbindung zur Datenbank wird gestartet");
 		Class.forName("com.mysql.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://192.168.111.144:3306/bvs?" + "user=root&passwort=Kekskuchen");
+		con = DriverManager
+				.getConnection("jdbc:mysql://192.168.111.144:3306/bvs?" + "user=bvsroot&password=Kekskuchen");
 		log.info("Verbindung erfolgreich");
 	}
-	
+
 	public Connection getConnection() {
-		
+
 		log.info("Die Datenbankverbindung wird hergestellt");
 		return this.con;
 	}
-	
+
 	public void closeConnection() throws SQLException {
-		
+
 		log.info("Die Datenbankverbindung wird geschlossen");
 		con.close();
 	}
