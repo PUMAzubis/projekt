4cf842a413d33d47aac5a18192765a9868c5a8a4
FlSeidl <FlSeidl@192.168.111.121>
2017-02-15 11:51:24 +0100

    Book DAO auf Deutsch gefixt, PasswordAuthentication hinzugefügt

--- src/de/dpma/pumaz/bvs/dao/BookDAO.java
+++ src/de/dpma/pumaz/bvs/dao/BookDAO.java
@@ -10,22 +10,22 @@ import java.util.List;
 import de.dpma.pumaz.bvs.model.Book;
 
 public class BookDAO {
-	final String INSERT_BUCH = "INSERT INTO `books` (`name`, `author`, `release_year`, `isbn`, `id_category`) VALUES (?, ?, ?, ?, ?)";
-	final String INSERT_BUCH_INSTANZ = "INSERT INTO `books_single` (`id_books`, `id_borrower`, `available`) VALUES (?, ?, ?)";
+	final String INSERT_BUCH = "INSERT INTO `books` (`name`, `author`, `release_year`, `isbn`, `id_categorys`) VALUES (?, ?, ?, ?, ?)";
+	final String INSERT_BUCH_INSTANCE = "INSERT INTO `books_single` (`id_books`, `id_borrower`, `available`) VALUES (?, ?, ?)";
 
 	final String DELETE_BUCH = "DELETE FROM `books` WHERE `id` = ?";
-	final String DELETE_BUCH_INSTANZ = "DELETE FROM `books_single` WHERE `single_id` = ?";
+	final String DELETE_BUCH_INSTANCE = "DELETE FROM `books_single` WHERE `single_id` = ?";
 
-	final String UPDATE_BUCH = "UPDATE `books` SET `name` = ?, `author` = ?, `release_year` = ?, `isbn` = ?, `id_category` = ? WHERE `id` = ?";
-	final String UPDATE_BUCH_INSTANZ = "UPDATE `books_single` SET `id_books` = ?, `id_borrower` = ?, `available` = ? WHERE `single_id` = ?";
+	final String UPDATE_BUCH = "UPDATE `books` SET `name` = ?, `author` = ?, `release_year` = ?, `isbn` = ?, `id_categorys` = ? WHERE `id` = ?";
+	final String UPDATE_BUCH_INSTANCE = "UPDATE `books_single` SET `id_books` = ?, `id_borrower` = ?, `available` = ? WHERE `single_id` = ?";
 
 	// SELECT_BUCH ohne ALL Noch nicht implementiert
 	final String SELECT_BUCH = "SELECT *, (SELECT COUNT(*) FROM `books_single` WHERE b.`id` = 1) as count, (SELECT COUNT(*) FROM `books_single` WHERE b.`id` = 1 AND `available` = 1) as available_count FROM `books` b WHERE `id` = ?";
-	final String SELECT_BUCH_INSTANZ = "SELECT * FROM `books_single` WHERE `id` = ?";
+	final String SELECT_BUCH_INSTANCE = "SELECT * FROM `books_single` WHERE `id` = ?";
 	final String SELECT_BUCH_TOGETHER = "SELECT * FROM `books` b1 JOIN `books_single` b2 ON b2.`id_books` = b1.`id` WHERE b2.`id` = ?";
 
 	final String SELECT_BUCH_ALL = "SELECT *, (SELECT COUNT(*) FROM `books_single` WHERE b.`id` = 1) as count, (SELECT COUNT(*) FROM `books_single` WHERE b.`id` = 1 AND `available` = 1) as available_count FROM `books` b";
-	final String SELECT_BUCH_ALL_INSTANZ = "SELECT * FROM `books_single`";
+	final String SELECT_BUCH_ALL_INSTANCE = "SELECT * FROM `books_single`";
 	final String SELECT_BUCH_ALL_TOGETHER = "SELECT * FROM `books` b1 JOIN `books_single` b2 ON b2.`id_books` = b1.`id`";
 
 	private final Connection con;
@@ -34,7 +34,7 @@ public class BookDAO {
 		this.con = con;
 	}
 
-	public Book insertBuch(Book b) throws SQLException {
+	public Book insertBook(Book b) throws SQLException {
 		PreparedStatement stat = con.prepareStatement(INSERT_BUCH);
 		stat.setString(1, b.getName());
 		stat.setString(2, b.getAuthor());
@@ -45,8 +45,8 @@ public class BookDAO {
 		return b;
 	}
 
-	public Book insertBuchInstanz(Book b) throws SQLException {
-		PreparedStatement stat = con.prepareStatement(INSERT_BUCH_INSTANZ);
+	public Book insertBookInstance(Book b) throws SQLException {
+		PreparedStatement stat = con.prepareStatement(INSERT_BUCH_INSTANCE);
 		stat.setInt(1, b.getBooks_single_id_books());
 		stat.setInt(2, b.getBooks_single_id_borrower());
 		stat.setInt(3, b.getBooks_single_available());
@@ -54,7 +54,7 @@ public class BookDAO {
 		return b;
 	}
 
-	public Book updateBuch(Book b) throws SQLException {
+	public Book updateBook(Book b) throws SQLException {
 		PreparedStatement stat = con.prepareStatement(UPDATE_BUCH);
 		stat.setString(1, b.getName());
 		stat.setString(2, b.getAuthor());
@@ -66,8 +66,8 @@ public class BookDAO {
 		return b;
 	}
 
-	public Book updateBuchInstanz(Book b) throws SQLException {
-		PreparedStatement stat = con.prepareStatement(UPDATE_BUCH_INSTANZ);
+	public Book updateBookInstance(Book b) throws SQLException {
+		PreparedStatement stat = con.prepareStatement(UPDATE_BUCH_INSTANCE);
 		stat.setInt(1, b.getBooks_single_id_books());
 		stat.setInt(2, b.getBooks_single_id_borrower());
 		stat.setInt(3, b.getBooks_single_available());
@@ -76,25 +76,25 @@ public class BookDAO {
 		return b;
 	}
 
-	public Book deleteBuch(Book b) throws SQLException {
+	public Book deleteBook(Book b) throws SQLException {
 		PreparedStatement stat = con.prepareStatement(DELETE_BUCH);
 		stat.setInt(1, b.getId());
 		stat.executeUpdate();
 		return b;
 	}
 
-	public Book deleteBuchInstanz(Book b) throws SQLException {
-		PreparedStatement stat = con.prepareStatement(DELETE_BUCH_INSTANZ);
+	public Book deleteBookInstance(Book b) throws SQLException {
+		PreparedStatement stat = con.prepareStatement(DELETE_BUCH_INSTANCE);
 		stat.setInt(1, b.getBooks_single_id());
 		stat.executeUpdate();
 		return b;
 	}
 
-	public List<Book> alleBuecher() throws SQLException {
+	public List<Book> allBooks() throws SQLException {
 		PreparedStatement stat = con.prepareStatement(SELECT_BUCH_ALL);
 		ResultSet result = stat.executeQuery();
 
-		ArrayList<Book> Buecher = new ArrayList<>();
+		ArrayList<Book> Books = new ArrayList<>();
 		while (result.next()) {
 			Book Book = new Book();
 			Book.setCount(result.getInt("count"));
@@ -106,32 +106,32 @@ public class BookDAO {
 			Book.setRelease_year(result.getInt("release_year"));
 			Book.setISBN(result.getString("isbn"));
 			Book.setId_categorys(result.getInt("id_categorys"));
-			Buecher.add(Book);
+			Books.add(Book);
 		}
-		return Buecher;
+		return Books;
 	}
 
-	public List<Book> alleBuecherInstanz() throws SQLException {
-		PreparedStatement stat = con.prepareStatement(SELECT_BUCH_ALL_INSTANZ);
+	public List<Book> allBooksInstance() throws SQLException {
+		PreparedStatement stat = con.prepareStatement(SELECT_BUCH_ALL_INSTANCE);
 		ResultSet result = stat.executeQuery();
 
-		ArrayList<Book> Buecher = new ArrayList<>();
+		ArrayList<Book> Books = new ArrayList<>();
 		while (result.next()) {
 			Book Book = new Book();
 			Book.setBooks_single_id(result.getInt("single_id"));
 			Book.setBooks_single_id_books(result.getInt("id_books"));
 			Book.setBooks_single_id_borrower(result.getInt("id_borrower"));
 			Book.setBooks_single_available(result.getInt("available"));
-			Buecher.add(Book);
+			Books.add(Book);
 		}
-		return Buecher;
+		return Books;
 	}
 
-	public List<Book> alleBuecherTogether() throws SQLException {
-		PreparedStatement stat = con.prepareStatement(SELECT_BUCH_ALL_INSTANZ);
+	public List<Book> allBooksTogether() throws SQLException {
+		PreparedStatement stat = con.prepareStatement(SELECT_BUCH_ALL_INSTANCE);
 		ResultSet result = stat.executeQuery();
 
-		ArrayList<Book> Buecher = new ArrayList<>();
+		ArrayList<Book> Books = new ArrayList<>();
 		while (result.next()) {
 			Book Book = new Book();
 			Book.setId(result.getInt("id"));
@@ -145,9 +145,9 @@ public class BookDAO {
 			Book.setBooks_single_id_books(result.getInt("id_books"));
 			Book.setBooks_single_id_borrower(result.getInt("id_borrower"));
 			Book.setBooks_single_available(result.getInt("available"));
-			Buecher.add(Book);
+			Books.add(Book);
 		}
-		return Buecher;
+		return Books;
 	}
 
 }
