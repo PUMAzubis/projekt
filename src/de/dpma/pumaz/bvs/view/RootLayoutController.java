dc5a3f95147a4da1e80e12f842ef082ff7a974fb
KeBöhmer <KeBhmer@192.168.111.105>
2017-02-16 11:11:58 +0100

    RootLayoutController @FXML hinzugefügt Login.fxml DPMA Logo gelöscht
    Kenneth

--- src/de/dpma/pumaz/bvs/view/RootLayoutController.java
+++ src/de/dpma/pumaz/bvs/view/RootLayoutController.java
@@ -4,43 +4,45 @@ import java.sql.SQLException;
 
 import de.dpma.pumaz.bvs.FXML_GUI;
 import de.dpma.pumaz.bvs.MainApp;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
 
 public class RootLayoutController {
-
+	
 	private int height;
-
+	
 	private int width;
-
+	
 	FXML_GUI fxml_gui;
-
+	
 	public void getSizes(int height, int width) {
-
+		
 		this.height = height;
 		this.width = width;
-
+		
 	}
-
+	
 	public void reziseHandling() {
-
+		
 		fxml_gui.rootLayout.setMinSize(width, height);
 		fxml_gui.rootLayout.setPrefSize(width, height);
 	}
-
+	
+	@FXML
 	public void handleExit() {
-
+		
 		try {
 			MainApp.dbcon.closeConnection();
 			System.exit(0);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	
+	@FXML
 	public void handleAbout() {
-
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(LoginController.class.getResource("RootLayout.fxml"));
+		
+		fxml_gui.showAbout();
 	}
 }
