1bd6da3a520c153340c93a7feb99390fdb5cba4c
KeBöhmer <KeBhmer@192.168.111.145>
2017-02-16 16:15:56 +0100

    Login verknüpfung hinzugefügt für das Registrieren Registrierfenster versuch zu öffnen MenuEmployee DPMA Logo gelöscht RootLayoutController About Funktion eingefügt (nicht fertig)
    Kenneth

--- src/de/dpma/pumaz/bvs/view/LoginController.java
+++ src/de/dpma/pumaz/bvs/view/LoginController.java
@@ -1,72 +1,92 @@
 package de.dpma.pumaz.bvs.view;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
 import java.util.logging.Logger;
 
+import de.dpma.pumaz.bvs.FXML_GUI;
 import de.dpma.pumaz.bvs.MainApp;
 import de.dpma.pumaz.bvs.dao.UserDAO;
 import de.dpma.pumaz.bvs.model.User;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class LoginController {
-
+	
 	@FXML
 	private TextField identificationNumberText;
-
+	
 	@FXML
 	private TextField passwordText;
-
+	
+	FXML_GUI fxml_gui;
+	
 	User loginUser;
-
+	
 	Logger log = Logger.getLogger(LoginController.class.getName());
-
+	
 	// Eingaben prüfen und Nutzer einloggen
 	public void handleLogin() throws SQLException {
-
+		
 		if (identificationNumberText.getText().isEmpty() || !isNumeric(identificationNumberText.getText())
 				|| identificationNumberText.getText().length() > 5) {
 			// TODO: Alert anzeigen
 			log.info("Keine gültige Ausweisnummer");
 			return;
 		}
-
+		
 		if (passwordText.getText().isEmpty() || passwordText.getText().length() < 8) {
 			// TODO: Alert anzeigen
 			log.info("Kein gültiges Passwort");
 			return;
 		}
-
+		
 		UserDAO UserDao = new UserDAO(MainApp.dbcon.getConnection());
 		loginUser = UserDao.findUser(new User(Integer.parseInt(identificationNumberText.getText())));
 		if (loginUser.getForename() == null) {
 			// TODO: Alert anzeigen
 			log.info("Mitarbeiter nicht gefunden");
-		} else {
+		}
+		else {
 			System.out.println("Mitarbeiter gefunden");
 			if (loginUser.checkPassword(passwordText.getText(), loginUser.getPassword())) {
 				// TODO: Weiterleiten
 				log.info("Passwort richtig, User einloggen");
-			} else {
+			}
+			else {
 				// TODO: Alert anzeigen
 				log.info("Passwort falsch");
 			}
 		}
 	}
-
+	
 	// Check ob String eine Zahl ist. Um die Ausweisnummer zu prüfen.
 	public static boolean isNumeric(String str) {
-
+		
 		NumberFormat formatter = NumberFormat.getInstance();
 		ParsePosition pos = new ParsePosition(0);
 		formatter.parse(str, pos);
 		return str.length() == pos.getIndex();
 	}
-
-	public void handleOpenRegister() {
-
+	
+	@FXML
+	public void handleRegistration() {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(FXML_GUI.class.getResource("view/Registration.fxml"));
+			AnchorPane register;
+			register = (AnchorPane) loader.load();
+			fxml_gui.rootLayout.setCenter(register);
+			fxml_gui.primaryStage.show();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 }
--- src/de/dpma/pumaz/bvs/view/RootLayoutController.java
+++ src/de/dpma/pumaz/bvs/view/RootLayoutController.java
@@ -1,10 +1,13 @@
 package de.dpma.pumaz.bvs.view;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 import de.dpma.pumaz.bvs.FXML_GUI;
 import de.dpma.pumaz.bvs.MainApp;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
 public class RootLayoutController {
 	
@@ -43,6 +46,42 @@ public class RootLayoutController {
 	@FXML
 	public void handleAbout() {
 		
-		fxml_gui.showAbout();
+		// try {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(FXML_GUI.class.getResource("view/About.fxml"));
+		// AnchorPane about;
+		// about = (AnchorPane) loader.load();
+		// Stage stage = new Stage();
+		// Scene scene = new Scene(about);
+		// stage.setScene(scene);
+		// stage.show();
+		// }
+		// catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		// fxml_gui.showAbout();
+		// fxml_gui.showLogin();
+	}
+	
+	@FXML
+	public void handleAnleitung() {
+		
+	}
+	
+	@FXML
+	public void handleRegistration() {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(FXML_GUI.class.getResource("view/Registration.fxml"));
+			AnchorPane register;
+			register = (AnchorPane) loader.load();
+			fxml_gui.rootLayout.setCenter(register);
+			fxml_gui.primaryStage.show();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
