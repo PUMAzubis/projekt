3241d165f7af6dc4c9fce8ec538a4586aaa81c73
FlSeidl <FlSeidl@192.168.111.121>
2017-02-16 10:17:44 +0100

    HandleAbout gefixt
    Flo

--- src/de/dpma/pumaz/bvs/view/RootLayoutController.java
+++ src/de/dpma/pumaz/bvs/view/RootLayoutController.java
@@ -4,37 +4,43 @@ import java.sql.SQLException;
 
 import de.dpma.pumaz.bvs.FXML_GUI;
 import de.dpma.pumaz.bvs.MainApp;
+import javafx.fxml.FXMLLoader;
 
 public class RootLayoutController {
-	
+
 	private int height;
-	
+
 	private int width;
-	
+
 	FXML_GUI fxml_gui;
-	
+
 	public void getSizes(int height, int width) {
-		
+
 		this.height = height;
 		this.width = width;
-		
+
 	}
-	
+
 	public void reziseHandling() {
-		
+
 		fxml_gui.rootLayout.setMinSize(width, height);
 		fxml_gui.rootLayout.setPrefSize(width, height);
 	}
-	
+
 	public void handleExit() {
-		
+
 		try {
 			MainApp.dbcon.closeConnection();
 			System.exit(0);
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public void handleAbout() {
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(LoginController.class.getResource("RootLayout.fxml"));
 	}
 }
