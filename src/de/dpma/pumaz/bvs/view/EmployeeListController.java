89b254c329e9c1f69b22d14193029c569a816bef
AbAl-Mas <AbAl-Mas@192.168.111.106>
2017-02-20 10:12:04 +0100

    Employee list controller bearbeitet
    Abdallah

--- src/de/dpma/pumaz/bvs/view/EmployeeListController.java
+++ src/de/dpma/pumaz/bvs/view/EmployeeListController.java
@@ -1,52 +1,60 @@
 package de.dpma.pumaz.bvs.view;
 
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import de.dpma.pumaz.bvs.FXML_GUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class EmployeeListController {
 
+
+	@FXML
+	private Label firstnameColumnLabel;
+	@FXML
+	private Label lastnameColumnLabel;
+	@FXML
+	private Label headerLabel;
+	@FXML
+	private Label detailsLabel;
+	@FXML
+	private Label idLabel;
+	@FXML
+	private Label firstnameLabel;
+	@FXML
+	private Label lastnameLabel;
+	@FXML
+	private Label workIDLabel;
+	@FXML
+	private Label statusLabel;
+	@FXML
+	private Button editButton;
+	@FXML
+	private Button newButton;
+	@FXML
+	private Button deleteButton;
+	@FXML
+	private Button cancelButton;
+
+	Stage stage = new Stage();
 	
-@FXML
-private Label firstnameColumnLabel;
+	FXML_GUI fxml_gui;
+	
+	Logger log = Logger.getLogger(EmployeeListController.class.getName());
 
-@FXML
-private Label lastnameColumnLabel;
-
-@FXML
-private Label headerLabel;
-
-@FXML
-private Label detailsLabel;
-
-@FXML
-private Label idLabel;
-
-@FXML
-private Label firstnameLabel;
-
-@FXML
-private Label lastnameLabel;
-
-@FXML
-private Label workIDLabel;
-
-@FXML
-private Label statusLabel;
-
-@FXML
-private Button editButton;
-
-@FXML
-private Button newButton;
-
-@FXML
-private Button deleteButton;
-
-@FXML
-private Button cancelButton;
-
-
-
+	public void handleEdit() throws SQLException{
+	}
+	
+	public void handleNew() throws SQLException{
+	}
+	
+	public void handleDelete() throws SQLException{
+	}
+	
+	public void handleBack() throws SQLException{
+	}
 
 }
