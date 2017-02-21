7052a12a8978038bbdd5fffc8d8333d78280add3
KeBöhmer <KeBhmer@KennethBoehmer>
2017-02-21 15:26:17 +0100

    BookListController abrufbar
    Kenneth

--- src/de/dpma/pumaz/bvs/view/BookListController.java
+++ src/de/dpma/pumaz/bvs/view/BookListController.java
@@ -16,150 +16,158 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class BookListController {
-
+	
 	@FXML
 	private Tooltip textFieldSearchToolTip;
-
+	
 	@FXML
 	private Tooltip buttonLendToolTip;
-
+	
 	@FXML
 	private Tooltip buttonEditToolTip;
-
+	
 	@FXML
 	private Tooltip buttonNewBookToolTip;
-
+	
 	@FXML
 	private Tooltip buttonDeleteToolTip;
+	
 	@FXML
-	private Label headerLabel;
-
+	private Text headerLabel;
+	
 	@FXML
 	private TableView<Book> bookTable;
-
+	
 	@FXML
 	private TableColumn<Book, String> nameColumn;
-
+	
 	@FXML
 	private TableColumn<Book, String> authorColumn;
-
+	
 	@FXML
 	private TableColumn<Book, String> releaseYearColumn;
-
+	
 	@FXML
 	private TableColumn<Book, String> isbnColumn;
-
+	
 	@FXML
 	private TableColumn<Book, String> categoryColumn;
-
+	
 	@FXML
 	private TableColumn<Book, String> availableColumn;
-
+	
 	@FXML
 	private TextField searchTextField;
-
+	
 	@FXML
 	private Button lendBookButton;
-
+	
 	@FXML
 	private Button editBookButtom;
-
+	
 	@FXML
 	private Button newBookButton;
-
+	
 	@FXML
 	private Button deleteBookButton;
-
+	
 	@FXML
 	private Label nameColumnLabel;
-
+	
 	@FXML
 	private Label authorColumnLabel;
-
+	
 	@FXML
 	private Label releaseColumnLabel;
-
+	
 	@FXML
 	private Label isbnColumnLabel;
-
+	
 	@FXML
 	private Label availableColumnLabel;
-
+	
 	@FXML
 	private Label categoryColumnLabel;
-
+	
 	@FXML
 	private Label searchLabel;
-
+	
 	public BookListController() {
-
-		textFieldSearchToolTip.setText("Type in title, year of release, author or ISBN to search for a book");
-		buttonLendToolTip.setText("Click to lend book");
-		buttonEditToolTip.setText("Click to edit");
-		buttonNewBookToolTip.setText("Click to create new book");
-		buttonDeleteToolTip.setText("Click to delete selected book");
-		headerLabel.setText("Menu");
-		nameColumn.setText("Name");
-		authorColumn.setText("Author");
-		releaseYearColumn.setText("Year of release");
-		isbnColumn.setText("ISBN");
-		categoryColumn.setText("Category");
-		searchTextField.setText("Search");
-		lendBookButton.setText("Lend");
-		editBookButtom.setText("Edit");
-		newBookButton.setText("New");
-		deleteBookButton.setText("Delete");
-		nameColumnLabel.setText("Name");
-		authorColumnLabel.setText("Author");
-		releaseColumnLabel.setText("Year of release");
-		isbnColumnLabel.setText("ISBN");
-		categoryColumnLabel.setText("Category");
-		searchLabel.setText("Search");
-
-		textFieldSearchToolTip.setText("Hier den Titel, ISBN, Jahr, oder Autor des Buches eingeben");
-		buttonLendToolTip.setText("Hier klicken um das ausgewählte Buch zu verleihen.");
-		buttonEditToolTip.setText("Hier Klicken um das ausgewählte Buch zu bearbeiten.");
-		buttonNewBookToolTip.setText("Hier klicken um ein neues Buch anzulegen.");
-		buttonDeleteToolTip.setText("Hier klicken um das ausgewählte Buch zu löschen.");
-		headerLabel.setText("Menü");
-		nameColumn.setText("Name");
-		authorColumn.setText("Autor");
-		releaseYearColumn.setText("Erscheinungsjahr");
-		isbnColumn.setText("ISBN");
-		categoryColumn.setText("Kategorie");
-		searchTextField.setText("Suche");
-		lendBookButton.setText("Verleihen");
-		editBookButtom.setText("Bearbeiten");
-		newBookButton.setText("Neu");
-		deleteBookButton.setText("Löschen");
-		nameColumnLabel.setText("Name");
-		authorColumnLabel.setText("Autor");
-		releaseColumnLabel.setText("Erscheinungsjahr");
-		isbnColumnLabel.setText("ISBN");
-		categoryColumnLabel.setText("Kategorie");
-		searchLabel.setText("Suche");
+		
+		// textFieldSearchToolTip.setText("Type in title, year of release,
+		// author or ISBN to search for a book");
+		// buttonLendToolTip.setText("Click to lend book");
+		// buttonEditToolTip.setText("Click to edit");
+		// buttonNewBookToolTip.setText("Click to create new book");
+		// buttonDeleteToolTip.setText("Click to delete selected book");
+		// headerLabel.setText("Menu");
+		// nameColumn.setText("Name");
+		// authorColumn.setText("Author");
+		// releaseYearColumn.setText("Year of release");
+		// isbnColumn.setText("ISBN");
+		// categoryColumn.setText("Category");
+		// searchTextField.setText("Search");
+		// lendBookButton.setText("Lend");
+		// editBookButtom.setText("Edit");
+		// newBookButton.setText("New");
+		// deleteBookButton.setText("Delete");
+		// nameColumnLabel.setText("Name");
+		// authorColumnLabel.setText("Author");
+		// releaseColumnLabel.setText("Year of release");
+		// isbnColumnLabel.setText("ISBN");
+		// categoryColumnLabel.setText("Category");
+		// searchLabel.setText("Search");
+		//
+		// textFieldSearchToolTip.setText("Hier den Titel, ISBN, Jahr, oder
+		// Autor des Buches eingeben");
+		// buttonLendToolTip.setText("Hier klicken um das ausgewählte Buch zu
+		// verleihen.");
+		// buttonEditToolTip.setText("Hier Klicken um das ausgewählte Buch zu
+		// bearbeiten.");
+		// buttonNewBookToolTip.setText("Hier klicken um ein neues Buch
+		// anzulegen.");
+		// buttonDeleteToolTip.setText("Hier klicken um das ausgewählte Buch zu
+		// löschen.");
+		// headerLabel.setText("Menü");
+		// nameColumn.setText("Name");
+		// authorColumn.setText("Autor");
+		// releaseYearColumn.setText("Erscheinungsjahr");
+		// isbnColumn.setText("ISBN");
+		// categoryColumn.setText("Kategorie");
+		// searchTextField.setText("Suche");
+		// lendBookButton.setText("Verleihen");
+		// editBookButtom.setText("Bearbeiten");
+		// newBookButton.setText("Neu");
+		// deleteBookButton.setText("Löschen");
+		// nameColumnLabel.setText("Name");
+		// authorColumnLabel.setText("Autor");
+		// releaseColumnLabel.setText("Erscheinungsjahr");
+		// isbnColumnLabel.setText("ISBN");
+		// categoryColumnLabel.setText("Kategorie");
+		// searchLabel.setText("Suche");
 	}
-
+	
 	Stage stage = new Stage();
-
+	
 	BorderPane borderPane = new BorderPane();
-
+	
 	FXML_GUI fxml_gui;
-
+	
 	RootLayoutController root = new RootLayoutController();
-
+	
 	private ObservableList<Book> bookData = FXCollections.observableArrayList();
-
+	
 	@FXML
 	private void initialize() throws SQLException {
-
+		
 		BookDAO bookDao = new BookDAO(MainApp.dbcon.getConnection());
 		bookData = FXCollections.observableArrayList((bookDao.allBooks()));
 		bookTable.setItems(bookData);
-
+		
 		nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
 		authorColumn.setCellValueFactory(cellData -> cellData.getValue().authorProperty());
 		releaseYearColumn.setCellValueFactory(cellData -> cellData.getValue().release_yearProperty());
@@ -167,18 +175,19 @@ public class BookListController {
 		categoryColumn.setCellValueFactory(cellData -> cellData.getValue().categoryName());
 		availableColumn.setCellValueFactory(cellData -> cellData.getValue().overviewCountProperty());
 	}
-
+	
 	@FXML
 	private void searchBooks() throws SQLException {
-
+		
 		if (searchTextField.getText().equals("")) {
 			initialize();
-		} else {
+		}
+		else {
 			BookDAO bookDao = new BookDAO(MainApp.dbcon.getConnection());
 			bookData = FXCollections.observableArrayList((bookDao.searchBooks(searchTextField.getText())));
 			System.out.println(searchTextField.getText());
 			bookTable.setItems(bookData);
-
+			
 			nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
 			authorColumn.setCellValueFactory(cellData -> cellData.getValue().authorProperty());
 			releaseYearColumn.setCellValueFactory(cellData -> cellData.getValue().release_yearProperty());
@@ -187,26 +196,26 @@ public class BookListController {
 			availableColumn.setCellValueFactory(cellData -> cellData.getValue().overviewCountProperty());
 		}
 	}
-
+	
 	@FXML
 	public void handleNewBook() {
-
+		
 		root.handleGUI("newBook");
 	}
-
+	
 	@FXML
 	public void handleEditBook() {
-
+		
 		root.handleGUI("editBook");
 	}
-
+	
 	@FXML
 	public void handleLendBook() {
-
+		
 	}
-
+	
 	@FXML
 	public void handleDeleteBook() {
-
+		
 	}
 }
